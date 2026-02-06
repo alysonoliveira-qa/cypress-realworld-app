9bd3f91806c03e78c46e9a40a5edc314b512f21a
alysonoliveira-qa <alyson.oliveira3d@gmail.com>
2026-02-06 18:36:22 -0300

    test(auth): improve login spec using shared user fixtures

---
+++ cypress/fixtures/users.ts
@@ -0,0 +1,16 @@
+export const users = {
+  validUser: {
+    username: 'Heath93',
+    password: 's3cret'
+  },
+
+  invalidUser: {
+    username: 'fakeUser',
+    password: 'wrongPass'
+  },
+
+  friendsList: {
+    friend1: 'Lia Rosenbaum'
+
+  }
+}
