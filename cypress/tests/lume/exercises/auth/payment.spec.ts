b4e4014c6a54a9f42069355ffe77537d8441a389
alysonoliveira-qa <alyson.oliveira3d@gmail.com>
2026-02-08 01:37:27 -0300

    test(payment): money transfer scenarios with overpayment

--- cypress/tests/lume/exercises/auth/payment.spec.ts
+++ cypress/tests/lume/exercises/auth/payment.spec.ts
@@ -2,8 +2,8 @@ import { users } from "../../../../fixtures/users"
 import { generateAmount } from "../../../../support/utility"
 
 
-describe('CT001-PG Enviar dinheiro com saldo suficiente', () => {
-  it('Deve enviar dinheiro com sucesso', () => {
+describe('Deve enviar dinheiro com sucesso', () => {
+  it('CT001-PG Enviar dinheiro com saldo suficiente', () => {
     // Arrange
     const amount = generateAmount(1,100)
     cy.visit('http://localhost:3000')
@@ -22,6 +22,29 @@ describe('CT001-PG Enviar dinheiro com saldo suficiente', () => {
     // Assert
     cy.contains('Transaction Submitted!').should('be.visible')
     cy.contains(`Paid $${amount}.00`).should('be.visible')
+    cy.url().should('include', '/transaction')
+  })
+
+  it.only('CT002-PG Enviar dinheiro com saldo insuficiente', () => {
+    // Arrange
+    const overAmount = generateAmount(10000,100000)
+    //const accountBalance ='[data-test="sidenav-user-balance"]'
+    cy.visit('http://localhost:3000')
+    cy.login(users.validUser.username, users.validUser.password)
+
+    // Act
+    cy.get('[data-test="nav-top-new-transaction"]').click()
+
+    cy.get('[data-test="user-list-search-input"]').type(users.friendsList.friend1)
+    cy.contains(users.friendsList.friend1).click()
+
+    cy.get('[name="amount"]').type(overAmount.toString())
+    cy.get('.MuiInputBase-root > [name="description"]').type('Over Payment')
+    cy.get('[data-test="transaction-create-submit-payment"]').click()
+
+    // Assert
+    cy.contains('Transaction Submitted!').should('be.visible')
+    cy.contains("$0.00").should('be.visible')
     cy.url().should('include', '/transaction')
   })
 })
