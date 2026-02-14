e8f2ecf35c554bed661d7930d17060640674f949
alysonoliveira-qa <alyson.oliveira3d@gmail.com>
2026-02-14 02:01:56 -0300

    test(e2e): add transactions history

---
+++ cypress/tests/lume/exercises/auth/transactions.spec.ts
@@ -0,0 +1,38 @@
+import { users } from "../../../../fixtures/users"
+
+describe('Visualizar histórico de transações com sucesso', () => {
+    
+  it.only('CT01 - Deve visualizar detalhes de uma transação existente', () => {
+    // Arrange
+    cy.visit('http://localhost:3000')
+    cy.login(users.validUser.username, users.validUser.password)
+
+    // Act1
+    cy.get('[data-test="nav-top-new-transaction"]').click()
+    cy.get('[data-test="user-list-search-input"]').type(users.friendList.friend1)
+    cy.contains(users.friendList.friend1).click()
+
+    cy.get('[name="amount"]').type('50')
+    cy.get('.MuiInputBase-root > [name="description"]').type('Test ensure transaction exists')
+    cy.get('[data-test="transaction-create-submit-payment"]').click()
+
+    cy.contains('Transaction Submitted!').should('be.visible')
+
+    // Act2
+    cy.get('[data-test="new-transaction-return-to-transactions"]').click()
+    cy.get('[data-test="nav-personal-tab"]').click()
+    cy.contains(users.friendList.friend1).click()
+
+    // Assert
+    cy.contains('Transaction Detail').should('be.visible')
+    cy.url().should('include', '/transaction')
+  })
+
+  it('CT02 - Deve exibir mensagem quando não houver transações', () => {
+
+
+    cy.contains('No Transactions').should('be.visible')
+
+  })
+
+})
